53edf415c479d8deac51f0422e71bde8145b2bf4
Lucy Wall <lhwall@gmail.com>
2018-03-26 18:29:47 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,7 +63,7 @@ def all_supplies_in_holidays(holiday_hash)
 holiday_hash.each do |season, holidays|
    puts "#{season.to_s.capitalize}:"
     holidays.each do |hdays, stuff|
-           puts "  #{hdays.to_s.capitalize}: #{stuff.join(", ").capitalize}"
+           puts "  #{hdays.to_s.capitalize}: #{stuff.capitalize.join(", ")}"
    end
  end
 end
